80795f54cb2fd559f823aec968df450a74a952c4
Rickey Hrabowskie <rickeyhrabowskie@Rickeys-MacBook-Pro.local>
2017-08-17 15:13:01 -0500

    Lesson 10.

--- Smack/Controller/CreateAccountVC.swift
+++ Smack/Controller/CreateAccountVC.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class CreateAccountVC: UIViewController {
-
+    
+    // Outlets
+    @IBOutlet weak var usernameTxt: UITextField!
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var passwordTxt: UITextField!
+    @IBOutlet weak var userImg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -18,5 +24,24 @@ class CreateAccountVC: UIViewController {
 
     @IBAction func closePressed(_ sender: UIButton) {
         performSegue(withIdentifier: UNWIND, sender: nil)
+    }
+    
+    @IBAction func createAccountPressed(_ sender: UIButton) {
+        guard let email = emailTxt.text, emailTxt.text != "" else { return }
+        guard let pass = passwordTxt.text, passwordTxt.text != "" else { return }
+        
+        AuthService.instance.registerUser(email: email, password: pass) { (success) in
+            if success {
+                print("registered user!")
+            }
+        }
+    }
+    
+    @IBAction func pickAvatarPressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func pickBGColorPressed(_ sender: UIButton) {
+        
     }
 }
