396c93b5115e132d6bc63f6278b7f037f7300181
Rickey Hrabowskie <rickeyhrabowskie@Rickeys-MacBook-Pro.local>
2017-08-17 16:43:26 -0500

    Lesson 12.

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -13,6 +13,8 @@ typealias CompletionHandler = (_ Success: Bool) -> ()
 // URL Constants
 let BASE_URL = "https://smack27.herokuapp.com/v1/"
 let URL_REGISTER = "\(BASE_URL)account/register"
+let URL_LOGIN = "\(BASE_URL)account/login"
+let URL_USER_ADD = "\(BASE_URL)user/add"
 
 // Segues
 let TO_LOGIN = "toLogin"
@@ -23,5 +25,10 @@ let UNWIND = "unwindToChannel"
 let TOKEN_KEY = "token"
 let LOGGED_IN_KEY = "loggedIn"
 let USER_EMAIL = "userEmail"
+
+// Headers
+let HEADER = [
+    "Content-Type": "application/json; charset=utf-8"
+]
 
 // https://smack27.herokuapp.com/
