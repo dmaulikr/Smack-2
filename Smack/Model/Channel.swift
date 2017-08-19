ff78dc0bc8cdfb158a3d267bdb94cdc47542b45f
Rickey Hrabowskie <rickeyhrabowskie@Rickeys-MacBook-Pro.local>
2017-08-19 03:06:43 -0500

    Lesson 19.

---
+++ Smack/Model/Channel.swift
@@ -0,0 +1,22 @@
+//
+//  Channel.swift
+//  Smack
+//
+//  Created by Rickey Hrabowskie on 8/19/17.
+//  Copyright © 2017 Rickey Hrabowskie. All rights reserved.
+//
+
+import Foundation
+
+struct Channel {
+    
+    public private(set) var channelTitle: String!
+    public private(set) var channelDescription: String!
+    public private(set) var id: String!
+    
+    // Using Decodable Protocol
+//    public private(set) var _id: String!
+//    public private(set) var name: String!
+//    public private(set) var description: String!
+//    public private(set) var __v: Int?
+}
